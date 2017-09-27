6260f515e761522687d0b0c73daff6513b5ef637
Francky Vercruysse <francky.vercruysse@outlook.com>
2017-09-28 00:44:08 +0200

    commit documentation

--- MyBlog1/Data/DbInitializer.cs
+++ MyBlog1/Data/DbInitializer.cs
@@ -1,8 +1,6 @@
 ﻿using MyBlog1.Models;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace MyBlog1.Data
 {
--- MyBlog1/Startup.cs
+++ MyBlog1/Startup.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MyBlog1.Data;
 using MyBlog1.Repositories;
-using Microsoft.EntityFrameworkCore;
 
 namespace MyBlog1
 {
