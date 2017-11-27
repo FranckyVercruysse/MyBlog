41e932e3062570ce7e627705196e28cadd88cf21
Francky Vercruysse <francky.vercruysse@outlook.com>
2017-11-27 17:00:11 +0100

    get started with Sass2

--- MyBlog/gulpfile.js
+++ MyBlog/gulpfile.js
@@ -3,8 +3,12 @@ This file is the main entry point for defining Gulp tasks and using Gulp plugins
 Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
 */
 
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    fs = require("fs"),
+    sass = require("gulp-sass");
 
-gulp.task('default', function () {
-    // place code for your default task here
+gulp.task('sass', function () {
+    return gulp.src('Styles/StyleSheet.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('wwwroot/css'));
 });
