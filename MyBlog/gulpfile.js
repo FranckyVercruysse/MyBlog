a963285381d8b3c3ffb0611475d287190629fff3
Francky Vercruysse <francky.vercruysse@outlook.com>
2017-11-29 08:44:50 +0100

    add custom.bootstrap.scss

--- MyBlog/gulpfile.js
+++ MyBlog/gulpfile.js
@@ -1,14 +1,13 @@
-﻿/*
+﻿/// <binding BeforeBuild='sass' />
+/*
 This file is the main entry point for defining Gulp tasks and using Gulp plugins.
 Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
 */
-
 var gulp = require('gulp'),
-    fs = require("fs"),
     sass = require("gulp-sass");
 
 gulp.task('sass', function () {
-    return gulp.src('Styles/StyleSheet.scss')
+    return gulp.src(['./wwwroot/lib/bootstrap/scss/bootstrap.scss','Styles/StyleSheet.scss'])
         .pipe(sass())
         .pipe(gulp.dest('wwwroot/css'));
 });
